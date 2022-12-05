3489187745cf343b67f4a1f367cf768d8942a519
Justin <levenebb@gmail.com>
2022-12-05 12:33:30 -0500

    Level 2

--- front-end/src/pages/Movies.js
+++ front-end/src/pages/Movies.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useContext } from "react";
 
 const Movies = () => {
   const [movieData, setMovieData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   
   //Fetch movie list
   useEffect(() => {
@@ -14,27 +15,40 @@ const Movies = () => {
         })
   }, [])
 
-  //sort alphabetically
-  const sortMovies = (movieArray) => {
-    return movieArray.sort((a, b) => {
-      if (a.title) {
-        const titleA = a.title.toUpperCase();
-        const titleB = b.title.toUpperCase();
-        if (titleA < titleB) {return -1};
-        if (titleA > titleB) {return 1};
-        return 0;
-      } 
-    })
-  }
-
     return (
+    <>
+    <div className="search-container">
+        <input 
+        className="movie-search-bar" 
+        type='text' 
+        placeholder="Search Movie List" 
+        onChange={(event) => {
+            setSearchTerm(event.target.value);
+        }}
+        /> 
+    </div>
     <div className='movielist-container'>
-        {sortMovies([...movieData]).map(movie => (
+    {movieData.filter((value) => {
+    if (searchTerm == "" ) {
+      return value
+    } else if (value.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+      return value
+    }
+  }).sort((a, b) => {
+    if (a.title) {
+      const titleA = a.title.toUpperCase();
+      const titleB = b.title.toUpperCase();
+      if (titleA < titleB) {return -1};
+      if (titleA > titleB) {return 1};
+      return 0;
+    } 
+  }).map(movie => (
             <div id='movie.id'>
                 <h2>{movie.title}</h2>
             </div>
         ))}
     </div>
+    </>
   )
 }
 
